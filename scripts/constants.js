911b1ccedcffa52521a6102bad44d2a7295d55fb
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-22 21:35:52 +0200

    add todo for optimise player-global-score::after

--- scripts/constants.js
+++ scripts/constants.js
@@ -5,7 +5,10 @@ export const HoldButton = document.getElementById("btn-hold");
 export const NewGameButton = document.getElementById("btn-new-game");
 
 // Scores
+export const PlayerMain = document.getElementById("player-main");
 export const PlayerMainGlobalScore = document.getElementById("player-main-global-score-value");
 export const PlayerMainCurrentScore = document.getElementById("player-main-current-score-value");
+
+export const PlayerSecond = document.getElementById("player-second");
 export const PlayerSecondGlobalScore = document.getElementById("player-second-global-score-value");
 export const PlayerSecondCurrentScore = document.getElementById("player-second-current-score-value");
--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,7 @@ import * as pkg from "./_imports.js";
 export function NewGame() {
     ResetAllScores();
     pkg.Index.EnabledButton();
+    SetFirstPlayer();
 }
 
 function ResetAllScores() {
@@ -10,4 +11,8 @@ function ResetAllScores() {
     pkg.Constants.PlayerMainCurrentScore.innerHTML = "0";
     pkg.Constants.PlayerSecondGlobalScore.innerHTML = "0";
     pkg.Constants.PlayerSecondCurrentScore.innerHTML = "0";
+}
+
+function SetFirstPlayer() {
+    pkg.Utils.AddClassOnHtmlElement(pkg.Constants.PlayerMain, 'player-turn');
 }
