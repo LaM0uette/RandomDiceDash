7a2f84b1e025f398ddc40034643b72c51413d72b
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-23 14:12:24 +0200

    test fonts

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 import * as pkg from "./_imports.js";
 
 const mediaQuery = window.matchMedia("screen and (max-width: 999px)");
-
 function handleMediaQueryChange(event) {
     if (event.matches) {
         pkg.Game.SetOnMobile(true);
@@ -22,3 +21,18 @@ pkg.Constants.HoldButton.addEventListener("click", pkg.Game.Hold);
 function OnLoad() {
     pkg.Index.DisableButton();
 }
+
+function deleteAllCookies() {
+    const cookies = document.cookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const equalPos = cookie.indexOf('=');
+        const name = equalPos > -1 ? cookie.substr(0, equalPos) : cookie;
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/';
+    }
+}
+
+window.addEventListener('beforeunload', () => {
+    deleteAllCookies();
+});
