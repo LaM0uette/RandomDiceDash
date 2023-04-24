029528e2737e0a51a0c39cb43c95366c1b45bec0
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-24 10:53:55 +0200

    convert index to arrow functions

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,16 @@ import * as pkg from "./_imports.js";
 
 const elements = [pkg.Constants.DiceButton, pkg.Constants.RollButton, pkg.Constants.HoldButton]
 
-export function EnabledButton() {
+export const EnabledButton = () => {
     pkg.Utils.RemoveClassOnHtmlElements(elements, 'disabled-button');
     pkg.Constants.DiceButton.addEventListener("click", pkg.Game.RollDice);
     pkg.Constants.RollButton.addEventListener("click", pkg.Game.RollDice);
     pkg.Constants.HoldButton.addEventListener("click", pkg.Game.Hold);
-}
+};
 
-export function DisableButton() {
+export const DisableButton = () => {
     pkg.Utils.AddClassOnHtmlElements(elements, 'disabled-button');
     pkg.Constants.DiceButton.removeEventListener("click", pkg.Game.RollDice);
     pkg.Constants.RollButton.removeEventListener("click", pkg.Game.RollDice);
     pkg.Constants.HoldButton.removeEventListener("click", pkg.Game.Hold);
-}
+};
